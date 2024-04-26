b748140994c7a6fb54cab616d0ec2afd827917cd
OmkarTenkale <tenkaleomkar7@gmail.com>
2024-04-26 09:21:39 +0530

    Refractor naming

--- nodal/build.gradle.kts
+++ nodal/build.gradle.kts
@@ -35,7 +35,7 @@ kotlin {
 }
 
 android {
-    namespace = "org.jetbrains.kotlinx.multiplatform.library.template"
+    namespace = "dev.omkartenkale.nodal"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
